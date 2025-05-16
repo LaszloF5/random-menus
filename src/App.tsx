a24acdc50aa1155ed9ac1815914b539dbf8e92e2
LaszloF5 <fazekaslaszlo31@gmail.com>
2025-05-16 11:08:15 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  //https://forkify-api.herokuapp.com/
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+    </div>
+  );
+}
+
+export default App;
